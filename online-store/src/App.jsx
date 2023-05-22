72469bbf90a2404e13e491395510b530e8dba067
edwin1007 <edwin.leonardo.cuaran@gmail.com>
2023-05-22 01:28:43 -0500

    avance menu movil

--- online-store/src/App.jsx
+++ online-store/src/App.jsx
@@ -1,10 +1,34 @@
+
+import { RiMenu3Fill, RiUser3Line, RiAddLine, RiPieChartLine } from "react-icons/ri";
 import Sidebar from "./components/shared/Sidebar";
+
 
 function App() {
 
   return (
     <div className="bg-[#262837] w-full min-h-screen">
-      <Sidebar/>
+      <Sidebar />
+      {/** Menu movil */}
+
+      <nav className="bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-400 py-4 px-8 
+                      flex items-center justify-between rounded-tl-xl rounded-tr-xl">
+        <button>
+          <RiUser3Line />
+        </button>
+
+        <button>
+          <RiAddLine />
+        </button>
+
+        <button>
+          <RiPieChartLine /> 
+        </button>
+
+        <button>
+          <RiMenu3Fill className="text-white"/>
+        </button>
+
+      </nav>
     </div>
   );
 }
