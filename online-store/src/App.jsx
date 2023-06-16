fa344c920cd5ccca58d4c01755957bf3168d1dee
edwin1007 <edwin.leonardo.cuaran@gmail.com>
2023-06-15 23:48:17 -0500

    en progreso diseño de 'carrito'

--- online-store/src/App.jsx
+++ online-store/src/App.jsx
@@ -161,7 +161,57 @@ function App() {
         </div>
 
         {/**panel derecho */}
-        <div className="lg:col-span-2 fixed lg:static right-0">Carrito</div>
+        <div className="lg:col-span-2 fixed lg:static right-0 top-0 bg-[#1F1D2B] w-full h-full">
+          {/** Orders */}
+          <div className="relative pt-16 text-gray-300 p-8">
+            <RiCloseLine className="absolute left-4 top-4 p-3 box-content text-gray-300 bg-[#262837]
+                                  rounded-full text-xl "/>
+            <h1 className="text-2xl my-4"> Order #151416</h1>
+            {/*** Pills - Etiquetas (tres botones)*/}
+            <div className="flex items-center gap-4 flex-wrap mb-8">
+              <button className="bg-[#ec7c6a] text-white py-2 px-4 rounded-xl">
+                Dine In 
+              </button>
+
+              <button className=" text-[#ec7c6a] py-2 px-4 rounded-xl border border-gray-500">
+                To Go
+              </button>
+
+              <button className=" text-[#ec7c6a] py-2 px-4 rounded-xl border border-gray-500">
+                Delivery
+              </button>
+            </div>
+            {/**Carrito */}
+            <div>
+              <div className="grid grid-cols-6 mb-6">
+                <h5 className="col-span-4">Item</h5> 
+                <h5>Qty</h5>
+                <h5>Price</h5>
+              </div>
+            {/**Producto */}
+            <div className="bg-[#262837] p-4 rounded-xl">
+              <div className="grid grid-cols-6">
+                {/**Product description*/} 
+                <div className="col-span-4 flex items-center gap-3">
+                  <img src="comida.png" className="w-10 h-10 object-cover"/> 
+                  <div>                  
+                    <h5 className="text-sm">Spaicy seas...</h5>
+                    <p className="text-xs text-gray-500">$2.29</p>
+                  </div>  
+                </div>
+                {/** Quantity */}
+                <div className="text-center">
+                  <span>2</span>
+                </div>
+                {/** Price */}
+                <div>
+                  <span>$4.58</span>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>  
+        </div>
       </main>
 
     </div>
